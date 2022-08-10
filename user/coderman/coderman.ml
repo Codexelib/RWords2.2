85bbc03980c2103047e927cedff5b643e5737868
Codexelib <70470945+Codexelib@users.noreply.github.com>
2022-08-11 00:51:46 +0300

    Add files via upload

---
+++ user/coderman/coderman.ml
@@ -0,0 +1,159 @@
+havza
+sermek
+aparat
+dahil etmek
+örgü örmek
+savurmak
+kolej
+sosyalist
+akraba
+yüzde
+ortaya çıkmak
+madalya
+ayıklamak
+kadınsı
+cesaret etmek
+ek
+yuva
+lehçe
+tersi
+olgun
+azınlık
+berbat
+ücret
+afiş
+gemiye binmek
+betimlemek
+kurban
+ders
+mum
+dar
+kubbe
+çakıl
+topuk
+toplantı
+konsolide etmek
+cehalet
+hasat
+pamuk
+liman
+saygın
+öncelik
+yerinden etmek
+kayıt
+tava
+bulaştırmak
+ısrar etmek 1
+karşılık
+tehdit
+masum
+tanıklık etmek
+felç
+tecrübe
+ilham vermek
+farklılık
+şikayet
+giriş
+işbirliği
+iddialı
+yıkmak
+açıklamak
+müdür
+vazgeçirmek
+senaryo
+uzlaştırmak
+mürettebat
+katkı
+vites
+çalışan
+incinme
+ifade
+kitle
+lamba
+dayanmak
+kabuk
+gelin
+sorumluluk
+ata
+ütü
+kolaylık
+telif hakkı
+teneke
+ideoloji
+istila
+kazanmak
+aldatmak
+ayrımcılık yapmak
+fazlalık
+patlamak
+liderlik
+kibrit
+olay
+işgal etmek
+kavga
+kordon
+yük gemisi
+coşku
+çatlak
+birinci sınıf öğrencisi
+temel
+ampul
+yön
+özel
+boru
+tanımlamak
+eşik
+hasta
+kuru
+sigorta
+kademeli
+karışıklık
+bükmek
+düzenleme
+çalı
+tahmin etmek
+halk
+parlaklık
+akım
+etkilemek
+tuğla
+kalanlar
+gömmek
+kuş tüyü
+servis
+kompakt
+içeri
+asit
+güverte
+sözcü
+kalıp
+hisse
+fayans
+zincir
+konsantrasyon
+rulo
+yüzyıl
+paten
+mevcudiyet
+kötü şöhretli
+anayasa
+çizelge
+yönerge
+kuzen
+sonbahar
+sigortalamak
+kiralamak
+deneme
+durgunluk
+ısrar etmek 2
+düzine
+kızgınlık
+imparatorluk
+ırkçılık
+karşılamak
+belli olmak
+zanaat
+un
+icat
+yük
+önsezi
